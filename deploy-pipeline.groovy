3f547f3d5d963a71dfe0c649c272fd10518ec104
Ron-Kou <kourengang@163.com>
2023-03-17 14:53:07 +0800

    Update deploy-pipeline.groovy

--- deploy-pipeline.groovy
+++ deploy-pipeline.groovy
@@ -1,10 +1,22 @@
+pipeline {
+    agent any
+    stages {
+        stage('Init') { steps {
+            echo "Initing..."
+        } }
+        stage('Build') { steps {
+            echo "Build..."
+            sh 'mvn package'
+        }
+            post {
+                success{
+                    echo 'start archiving...'
+                    archiveArtifacts artifact: '**、target/*.war'
+                    echo 'archivie complete'
+                }
+            }
+        }
 
-pipeline{
-  agent any
-  stages{
-    stage('Init'){steps{echo "Initing..."}}
-    stage('Build'){steps{echo "Build..."}}
-    stage('Deploy'){steps{echo "Deploy complete."}}
-  }
-  
+    }
+
 }
