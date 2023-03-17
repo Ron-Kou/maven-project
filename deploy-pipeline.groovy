605aa0cfc38ea544be0ee28eb2290fb4de5519aa
Ron-Kou <kourengang@163.com>
2023-03-17 11:16:33 +0800

    Update deploy-pipeline.groovy

--- deploy-pipeline.groovy
+++ deploy-pipeline.groovy
@@ -1,1 +1,10 @@
 
+pipeline{
+  agent any
+  stages{
+    stage('Init'){steps{echo "Initing..."}}
+    stage('Build'){steps{echo "Build..."}}
+    stage('Deploy'){steps{echo "Deploy complete."}}
+  }
+  
+}
